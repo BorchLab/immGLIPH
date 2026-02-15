d048545785888a72957c8c328dc7f4001e2c1bfc
theHumanBorch <ncborch@gmail.com>
2026-02-15 05:58:45 -0600

    package documentation

---
+++ R/immGLIPH-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+#' @aliases immGLIPH-package
+#'
+#' @references
+#' Glanville, J. et al. (2017). Identifying specificity groups in the
+#' T cell receptor repertoire. \emph{Nature}, 547, 94--98.
+#' \doi{10.1038/nature22976}
+#'
+#' Huang, H. et al. (2020). Analyzing the Mycobacterium tuberculosis immune
+#' response by T-cell receptor clustering with GLIPH2 and genome-wide antigen
+#' screening. \emph{Nature Biotechnology}, 38, 1194--1202.
+#' \doi{10.1038/s41587-020-0505-4}
+"_PACKAGE"
